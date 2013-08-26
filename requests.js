9eb0c52e69e464215927bd92ed84a5228981997f
Daniel Bugl <daniel.bugl@touchlay.com>
2013-08-26 16:57:15 +0200

    returning type now

--- requests.js
+++ requests.js
@@ -19,31 +19,31 @@ var db = database.createDB('sync', function(err, _collection) {
 });
 
 exports.version = function(data) {
-    return {name: "TouchLay Sync", version: "0.0.1"};
+    return {type: 'result', data: {name: 'TouchLay Sync', version: '0.0.1'}};
 };
 
 exports.findById = function(data) {
     if (!collection) {
-        return {};
+        return {type: 'result', data: {}};
     }
     
     database.findOneById(collection, req.params.id, function(err, item) {
-        return item;
+        return {type: 'result', data: item};
     });
     
-    return {};
+    return {type: 'result'};
 };
 
 exports.findAll = function(data) {
     if (!collection) {
-        return [];
+        return {type: 'result', data: []};
     }
     
     collection.find().toArray(function(err, items) {
-        return items;
+        return {type: 'result', data: items};
     });
     
-    return [];
+    return {type: 'result', data: []};
 };
 
 exports.create = function(data) {
@@ -51,51 +51,51 @@ exports.create = function(data) {
         collection.insert({data: data.data}, {safe: true}, function(err, records) {
             if (err) {
                 console.log('Errors while inserting document: ' + JSON.stringify(err));
-                return {success: false, errors: err, message: 'Failed to create new session.'};
+                return {type: 'message', data: {success: false, errors: err, message: 'Failed to create new session.'}};
             } else {
                 console.log('Inserted record: ' + JSON.stringify(records[0]));
-                return {success: true, message: 'Successfully created new session.'};
+                return {type: 'message', data: {success: true, message: 'Successfully created new session.'}};
             }
         });
     }
     
-    return {success: false, message: 'No data.'};
+    return {type: 'message', data: {success: false, message: 'No data.'}};
 };
 
 exports.update = function(data) {
     if (!data.id) {
-        return {success: false, message: 'No ID.'};
+        return {type: 'message', data: {success: false, message: 'No ID.'}};
     }
     
     if (data.data) {
         collection.update({_id: database.ObjectID(data.id)}, {data: data.data}, {safe: true}, function(err) {
             if (err) {
                 console.log('Errors while updating document: ' + JSON.stringify(err));
-                return {success: false, errors: err, message: 'Failed to update session (does it exist?).'};
+                return {type: 'message', data: {success: false, errors: err, message: 'Failed to update session (does it exist?).'}};
             } else {
                 console.log('Updated record "' + data.id + '".');
-                return {success: true, message: 'Successfully updated session.'};
+                return {type: 'message', data: {success: true, message: 'Successfully updated session.'}};
             }
         });
     }
     
-    return {success: false, message: 'No data.'};
+    return {type: 'message', data: {success: false, message: 'No data.'}};
 };
 
 exports.destroy = function(data) {
     if (!data.id) {
-        return {success: false, message: 'No ID.'};
+        return {type: 'message', data: {success: false, message: 'No ID.'}};
     }
     
     collection.remove({_id: database.ObjectID(data.id)}, {w: 1}, function(err, removedDocs) {
         if (err) {
             console.log('Errors while removing document: ' + JSON.stringify(err));
-            return {success: false, errors: err, message: 'Failed to destroy session (does it exist?).'};
+            return {type: 'message', data: {success: false, errors: err, message: 'Failed to destroy session (does it exist?).'}};
         } else {
             console.log('Removed record "' + data.id + '".');
-            return {success: true, message: 'Successfully destroyed session.'};
+            return {type: 'message', data: {success: true, message: 'Successfully destroyed session.'}};
         }
     });
     
-    return {success: false, message: 'Unknown error.'};
+    return {type: 'message', data: {success: false, message: 'Unknown error.'}};
 };
